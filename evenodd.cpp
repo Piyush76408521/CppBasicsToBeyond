1236ec963e6038ee71ff1a3f47eee9b4c360d568
Piyush Deshmukh <piyush.deshmukh674@gmail.com>
2025-08-07 21:50:58 +0530

    Create evenodd.cpp
    This program shows that the number given  by the user is even or odd .

---
+++ evenodd.cpp
@@ -0,0 +1,13 @@
+#include <b its/stdc++.h>
+using namespace std;
+int main() {
+int n;                              // Declaration of the value.
+cin >> n;                           // Taking the number n.
+if(n % 2 == 0) {                     // Condition for even number.
+cout << "It is even number" << endl;
+}
+else {                                // If the number does not satisfy the condition of if statement then it print the else statement output.
+cout << "It is odd number" << endl;
+}
+return 0;                           // Exists the program.
+}
