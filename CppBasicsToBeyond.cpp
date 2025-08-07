63bee92438b22486c41b3a7ded887acf029d4245
Piyush Deshmukh <piyush.deshmukh674@gmail.com>
2025-08-07 20:31:51 +0530

    Create CppBasicsToBeyond.cpp
    This is a simple C++ program that prints the message "CppBasicsToBeyond" to the console.
    It is the first step in learning how to write and run C++ code.
    The program uses the standard input-output library (iostream) and the cout statement to display text on the screen.
    This example introduces how a basic C++ program is structured using main() function and ends with a return 0; statement.
    */

---
+++ CppBasicsToBeyond.cpp
@@ -0,0 +1,9 @@
+//My first C++ Program
+#include <bits/stdc++.h> //Includes almost all standard libraies of Cpp(not recommended for big projects).
+using namespace std; // Allows Usage of Standard names without std::prefix.
+int main() {
+  // Enter Your Code Here.
+  cout << "CppBasicsToBeyond" << endl; // Printing the value.
+                                      // Using of endl is for ending the line and start a new line in Cpp.
+return 0; // Exists the Program.
+}
