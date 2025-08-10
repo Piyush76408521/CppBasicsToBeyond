06130d13cd4975614ee7babacaa1e62dcabab5af
Piyush Deshmukh <piyush.deshmukh674@gmail.com>
2025-08-10 20:42:41 +0530

    Create  ifelseandelseif.cpp
    This Program Shows howwe can use if, else and else if statement in a simple way to write our cpp codes.

---
+++ ifelseandelseif.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int n;
+cin >> n;
+if(n%5==0) {
+cout << "Equal to 5 " << endl;
+} else if (n>5) {
+cout << "Greater than 5" << endl;
+} else {
+cout << "Less than 5" << endl;
+}
+return 0;
+}
